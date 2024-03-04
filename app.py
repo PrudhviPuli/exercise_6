b9f7e7c33a6b3086bdbb55c9050e43e3c29015a9
PrudhviPuli <69890008+PrudhviPuli@users.noreply.github.com>
2024-03-04 16:52:31 -0600

    Submission

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import string
 import random
+import sqlite3
 from datetime import datetime
-from flask import Flask, g
+from flask import *
 from functools import wraps
 
 app = Flask(__name__)
@@ -59,6 +60,18 @@ def page_not_found(e):
     return app.send_static_file('404.html'), 404
 
 
+def get_user_from_cookie(request):
+    user_id, password = request.cookies.get('user_id'), request.cookies.get('user_password')
+    return query_db('select * from users where id = ? and password = ?', (user_id, password), one=True) if user_id and password else None
+
+def authenticateUser(request):
+    api_key = request.headers.get('Api-Key')
+    if not api_key:
+        return None
+    
+    user = query_db('SELECT * FROM users WHERE api_key = ?', [api_key], one=True)
+    return user
+
 
 # -------------------------------- API ROUTES ----------------------------------
 
@@ -68,9 +81,136 @@ def page_not_found(e):
 # def login():
 #   ...
 
+@app.route("/api/signup", methods=["POST"])
+def signup():
+    user = new_user()
+    if user:
+        response_data = {
+            "api_key": user["api_key"],
+            "user_id": user["id"],
+            "user_name": user["name"],
+        }
+        return make_response(jsonify(response_data), 201)
+    else:
+        return make_response(jsonify({"error": "Unable to create the user!"}), 400)
+    
+@app.route("/api/login", methods=["POST"])
+def login():
+    name, password = request.headers.get("userName"), request.headers.get("password")
+    u = query_db(
+        "SELECT id, api_key, name FROM users WHERE name = ? AND password = ?",
+        [name, password],
+        one=True,
+    )
+    return (
+        {"api_key": u[1], "user_id": u[0], "user_name": u[2]} if u else {"api_key": ""}
+    )
+    
+@app.route("/api/rooms/new", methods=["POST"])
+def create_room():
+    user = authenticateUser(request)
+    if user:
+        name = "Unnamed Room " + "".join(random.choices(string.digits, k=6))
+        room = query_db(
+            "INSERT INTO rooms (name) VALUES (?) RETURNING id",
+            [name],
+            one=True
+        )
+        return {"room_id": room["id"]}
+    else:
+        return make_response(jsonify({"error": "Unauthorized"}), 401)
 
-# @app.route('/api/login')
-# def login():
-#   ... 
+@app.route("/api/room/messages", methods=["GET"])
+def get_all_messages():
+    if authenticateUser(request):
+        room_id = request.args.get("room_id")
+        msgs = query_db(
+            "SELECT m.id, u.name, m.body FROM messages m, users u WHERE m.room_id = ? AND m.user_id = u.id ORDER BY m.id",
+            [room_id],
+        )
+        if msgs == None:
+            return {}, 200
+        return {
+            msg[0]: {"id": msg[0], "name": msg[1], "body": msg[2]} for msg in msgs
+        }, 200
+    return {"Status": "Unauthorized"}, 401
 
-# ... etc
+@app.route("/api/room/post", methods=["POST"])
+def post_message():
+    if authenticateUser(request):
+        query_db(
+            "INSERT INTO messages (user_id, room_id, body) VALUES (?, ?, ?)",
+            [
+                request.headers.get("User-Id"),
+                request.args.get("room_id"),
+                request.args.get("body"),
+            ],
+        )
+        return {"status": "Success"}, 200
+    return {"Status": "Unauthorized"}, 401
+
+@app.route("/api/username/change", methods=["POST"])
+def update_username():
+    if authenticateUser(request):
+        temp = query_db(
+            "UPDATE users SET name = ? WHERE api_key = ? RETURNING id, name",
+            [request.args.get("user_name"), request.headers.get("Api-Key")],
+            one=True,
+        )
+        return {"name": temp["name"]} if temp else {}
+    return {"Status": "Unauthorized"}, 401
+
+@app.route("/api/password/change", methods=["POST"])
+def update_password():
+    if authenticateUser(request):
+        query_db(
+            "UPDATE users SET password = ? WHERE api_key = ?",
+            [request.args.get("password"), request.headers.get("Api-Key")],
+        )
+        return {}, 200
+    return {"Status": "Unauthorized"}, 401
+
+@app.route("/api/rooms", methods=["GET"])
+def get_all_rooms():
+    user = authenticateUser(request)
+    if user:
+        rooms = query_db("SELECT * FROM rooms")
+        return jsonify({room["id"]: {"name": room["name"]} for room in rooms})
+    else:
+        return make_response(jsonify({"error": "Unauthorized"}), 401)
+
+@app.route("/api/room/name/change", methods=["POST"])
+def update_room():
+    if authenticateUser(request):
+        query_db(
+            "UPDATE rooms SET name = ? WHERE id = ?",
+            [request.args.get("name"), request.args.get("room_id")],
+        )
+        return {}, 200
+    return {"Status": "Unauthorized"}, 401
+    
+@app.route("/api/signup/details", methods=["POST"])
+def signup_details():
+    user_details = request.get_json()
+    name = user_details.get("userName")
+    password = user_details.get("Password")
+    api_key = "".join(random.choices(string.ascii_lowercase + string.digits, k=40))
+    user = query_db(
+        "INSERT INTO users (name, password, api_key) VALUES (?, ?, ?) RETURNING id, name, api_key",
+        [name, password, api_key],
+        one=True,
+    )
+    if user:
+        return jsonify({
+            "api_key": user["api_key"],
+            "user_id": user["id"],
+            "user_name": user["name"],
+        })
+    else:
+        return make_response(jsonify({"error": "Unable to create the user!"}), 401)
+
+@app.route("/api/error", methods=["POST"])
+def panicError():
+    return make_response(jsonify({"error": "Not Found"}), 404)
+
+
--- static/script.js
+++ static/script.js
@@ -4,6 +4,56 @@ const PROFILE = document.querySelector(".profile");
 const LOGIN = document.querySelector(".login");
 const ROOM = document.querySelector(".room");
 
+const API_ENDPOINTS = {
+  MESSAGES_URL: "/api/room/messages",
+  SEND_MESSAGE_URL: "/api/room/post",
+  USERNAME_UPDATE_URL: "/api/username/change",
+  PASSWORD_UPDATE_URL: "/api/password/change",
+  ROOM_NAME_UPDATE_URL: "/api/room/name/change",
+  SIGNUP_URL: "/api/signup",
+  SIGNUP_DETAILS_URL: "/api/signup/details",
+  LOGIN_URL: "/api/login",
+  CREATE_NEW_ROOM_URL: "/api/rooms/new",
+  ROOMS_URL: "/api/rooms",
+  ERROR_URL: "/api/error"
+}
+
+let rooms = {};
+let currentPath = '';
+let currentRoomID = 0;
+
+let loginDict = {
+  userName: '',
+  password: ''
+};
+
+let getAllMsgsRequest = {
+  room_id: 0
+};
+
+let postRequest = {
+  room_id: 0,
+  body: ''
+};
+
+let postUpdateNameRequest = {
+  user_name: ''
+};
+
+let postUpdatePassRequest = {
+  Password: ''
+};
+
+let postUpdateRoomRequest = {
+  name: '',
+  room_id: 0
+};
+
+let signUpDetails = {
+  userName: '',
+  Password: ''
+};
+
 // Custom validation on the password reset fields
 const passwordField = document.querySelector(".profile input[name=password]");
 const repeatPasswordField = document.querySelector(".profile input[name=repeatPassword]");
@@ -12,6 +62,8 @@ const repeatPasswordMatches = () => {
   const r = repeatPassword.value;
   return p == r;
 };
+
+
 
 const checkPasswordRepeat = () => {
   const passwordField = document.querySelector(".profile input[name=password]");
@@ -23,33 +75,321 @@ const checkPasswordRepeat = () => {
   }
 }
 
+
 passwordField.addEventListener("input", checkPasswordRepeat);
 repeatPasswordField.addEventListener("input", checkPasswordRepeat);
 
-// TODO:  On page load, read the path and whether the user has valid credentials:
-//        - If they ask for the splash page ("/"), display it
-//        - If they ask for the login page ("/login") and don't have credentials, display it
-//        - If they ask for the login page ("/login") and have credentials, send them to "/"
-//        - If they ask for any other valid page ("/profile" or "/room") and do have credentials,
-//          show it to them
-//        - If they ask for any other valid page ("/profile" or "/room") and don't have
-//          credentials, send them to "/login", but remember where they were trying to go. If they
-//          login successfully, send them to their original destination
-//        - Hide all other pages
+async function performRequest(endPoint, requestBody, requestHeader, endType){
+  let url = endPoint + (Object.keys(requestBody).length > 0 
+                        ? ("?" + Object.keys(requestBody).map((key) => key+"="+encodeURIComponent(requestBody[key])).join("&")) 
+                        : "");
 
-// TODO:  When displaying a page, update the DOM to show the appropriate content for any element
-//        that currently contains a {{ }} placeholder. You do not have to parse variable names out
-//        of the curly  braces—they are for illustration only. You can just replace the contents
-//        of the parent element (and in fact can remove the {{}} from index.html if you want).
+  let urlHeaders = new Headers();
+  urlHeaders.append("Accept", "application/json");
+  urlHeaders.append("Content-Type", "application/json");
+  urlHeaders.append("Api-Key", localStorage.getItem('API-KEY'));
+  urlHeaders.append("User-Id", localStorage.getItem('User-Id'));
 
-// TODO:  Handle clicks on the UI elements.
-//        - Send API requests with fetch where appropriate.
-//        - Parse the results and update the page.
-//        - When the user goes to a new "page" ("/", "/login", "/profile", or "/room"), push it to
-//          History
+  Object.keys(requestHeader).forEach(function(key) {
+    urlHeaders.append(key, requestHeader[key]);
+  });
 
-// TODO:  When a user enters a room, start a process that queries for new chat messages every 0.1
-//        seconds. When the user leaves the room, cancel that process.
-//        (Hint: https://developer.mozilla.org/en-US/docs/Web/API/setInterval#return_value)
+  const myInit = {
+    method: endType,
+    headers: urlHeaders,
+  };
 
-// On page load, show the appropriate page and hide the others
+  data = await fetch(url, myInit);
+  jsonForm = await data.json();
+  return jsonForm
+}
+
+function showThisElement(elementToShow) {
+  const sections = [SPLASH, PROFILE, LOGIN, ROOM];
+
+  sections.forEach(section => {
+    section.classList.add("hide");
+  });
+
+  elementToShow.classList.remove("hide");
+
+  if (elementToShow !== ROOM) {
+    currentRoomID = 0;
+  }
+}
+
+function toggleElementVisibility(selector, shouldBeVisible) {
+  const element = document.querySelector(selector);
+  if (shouldBeVisible) {
+    element.classList.remove("hide");
+  } else {
+    element.classList.add("hide");
+  }
+}
+
+function loginPost() {
+  toggleElementVisibility(".loginHeader .loggedOut", false);
+  toggleElementVisibility(".signup", false);
+  toggleElementVisibility(".loginHeader .loggedIn", true);
+  toggleElementVisibility(".create", true);
+
+  const userName = localStorage.getItem('User-Name');
+  document.querySelectorAll('.username').forEach(usernameSpan => {
+    const commonInnerHTML = `<a onclick="updateDetails()" style="text-decoration: underline; cursor: pointer; color: blue;">${userName}</a>! <a class="logout" onclick="logoutUser()" style="text-decoration: underline; cursor: pointer;">(logout)</a>`;
+    usernameSpan.innerHTML = usernameSpan.innerHTML.includes("Welcome") ? `Welcome back, ${commonInnerHTML}` : commonInnerHTML;
+  });
+
+  const usernameInput = document.querySelector('input[name="username"]');
+  if (usernameInput) usernameInput.value = userName;
+}
+
+function logoutPost() {
+  toggleElementVisibility(".loginHeader .loggedIn", false);
+  toggleElementVisibility(".create", false);
+  toggleElementVisibility(".loginHeader .loggedOut", true);
+  toggleElementVisibility(".signup", true);
+}
+
+function hideByDefault() {
+  toggleElementVisibility('.editRoomName', false);
+  toggleElementVisibility('.displayRoomName', true);
+}
+
+function hidebyCls() {
+  document.querySelector('.login .failed').style.display = 'none';
+}
+
+function LoadURL(url) {
+  window.history.pushState({}, '', '/' + url);
+  router();
+}
+
+
+async function signUP() {
+  try {
+    const response = await performRequest(API_ENDPOINTS.SIGNUP_URL, {}, {}, 'POST');
+    if (response) {
+      localStorage.setItem('API-KEY', response.api_key);
+      localStorage.setItem('User-Id', response.user_id);
+      localStorage.setItem('User-Name', response.user_name);
+      LoadURL('');
+    } else {
+      console.error('Signup failed: No response from server');
+    }
+  } catch (error) {
+    console.error('Signup error:', error);
+  }
+}
+
+async function signUPWithDetails() {
+  try {
+    signUpDetails.userName = document.getElementById('username').value;
+    signUpDetails.Password = document.getElementById('password').value;
+    const response = await performRequest(API_ENDPOINTS.SIGNUP_DETAILS_URL, {}, signUpDetails, 'POST');
+    if (response) {
+      localStorage.setItem('API-KEY', response.api_key);
+      localStorage.setItem('User-Id', response.user_id);
+      localStorage.setItem('User-Name', response.user_name);
+      LoadURL('');
+    } else {
+      console.error('Signup with details failed: No response from server');
+    }
+  } catch (error) {
+    console.error('Signup with details error:', error);
+  }
+}
+
+function logoutUser() {
+  localStorage.removeItem('API-KEY');
+  localStorage.removeItem('User-Id');
+  localStorage.removeItem('User-Name');
+  rooms = {};
+  document.querySelector(".roomList").innerHTML = '';
+  document.querySelector('.noRooms').style.display = "block";
+  LoadURL('');
+}
+
+async function loginUser() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  const response = await performRequest(API_ENDPOINTS.LOGIN_URL, {}, { userName: username, password: password }, 'POST');
+  
+  if (response && response.api_key) {
+    localStorage.setItem('API-KEY', response.api_key);
+    localStorage.setItem('User-Id', response.user_id);
+    localStorage.setItem('User-Name', response.user_name);
+    LoadURL(currentPath
+    
+     ? currentPath
+    
+     : '/');
+  } else {
+    document.querySelector('.login .failed').style.display = 'flex';
+  }
+}
+
+async function updateUsername() {
+  const newUsername = document.querySelector('input[name="username"]').value;
+  const response = await performRequest(API_ENDPOINTS.USERNAME_UPDATE_URL, { user_name: newUsername }, {}, 'POST');
+  if (response && response.name) {
+    localStorage.setItem('User-Name', response.name);
+    loginPost();
+  }
+}
+
+async function updatePassword() {
+  const newPassword = document.querySelector('input[name="password"]').value;
+  const repeatPassword = document.querySelector('input[name="repeatPassword"]').value;
+  if (newPassword === repeatPassword) {
+    const response = await performRequest(API_ENDPOINTS.PASSWORD_UPDATE_URL, { password: newPassword }, {}, 'POST');
+    if (response) {
+      loginPost();
+    }
+  } else {
+    console.error("Passwords do not match.");
+  }
+}
+
+async function createNewRoom() {
+  const response = await performRequest(API_ENDPOINTS.CREATE_NEW_ROOM_URL, {}, {}, 'POST');
+  LoadURL('room/' + response.room_id);
+}
+
+async function postMessage(body) {
+  const response = await performRequest(API_ENDPOINTS.SEND_MESSAGE_URL, { room_id: currentRoomID, body: body }, {}, 'POST');
+  if (response) {
+    document.getElementById("commentTA").value = '';
+  }
+}
+
+function loadRoom(roomId) {
+  LoadURL("room/" + roomId);
+}
+
+function toggleEditMode() {
+  document.querySelector('.displayRoomName').classList.add('hide');
+  document.querySelector('.editRoomName').classList.remove('hide');
+}
+
+async function populateRooms() {
+  rooms = await performRequest(API_ENDPOINTS.ROOMS_URL, {}, {}, 'GET');
+  const roomsDiv = document.querySelector(".roomList");
+  roomsDiv.innerHTML = ''; // Clear existing rooms
+
+  const noRoomsDiv = document.querySelector('.noRooms');
+  noRoomsDiv.style.display = rooms && Object.keys(rooms).length > 0 ? "none" : "block";
+
+  if (rooms) {
+    Object.entries(rooms).forEach(([roomId, roomDetails]) => {
+      const roomLink = document.createElement("a");
+      roomLink.href = "#";
+      roomLink.innerHTML = `${roomId}: <strong>${roomDetails.name}</strong>`;
+      roomLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        loadRoom(roomId);
+      });
+      roomsDiv.appendChild(roomLink);
+    });
+  }
+}
+
+async function saveRoomName() {
+  const roomName = document.querySelector('#roomNameInput').value;
+  await performRequest(API_ENDPOINTS.ROOM_NAME_UPDATE_URL, { name: roomName, room_id: currentRoomID }, {}, 'POST');
+  document.querySelector('.displayRoomName strong').textContent = roomName;
+  document.querySelector('.editRoomName').classList.add('hide');
+  document.querySelector('.displayRoomName').classList.remove('hide');
+}
+
+async function updateDetails() {
+  LoadURL('profile');
+}
+
+let router = async () => {
+  let path = window.location.pathname;
+  if(localStorage.getItem('API-KEY') == null){
+    logoutPost();
+    if(path != "/" && path.length > 1){
+      splitted = path.split('/');
+      currentPath
+    
+     = splitted[1];
+      for(var j = 2; j < splitted.length; j++){
+        currentPath
+      
+       += '/' + splitted[j];
+      }
+      window.history.pushState(null, null, '/login');
+      path = '/login';
+    }
+  }
+  else{
+    loginPost();
+
+    if(path == "/login"){
+      window.history.pushState(null, null, '/');
+      path = '/';
+    }
+  }
+  hideByDefault();
+  hidebyCls();
+
+  if(path == "/" || path == "/room") {
+    document.title = 'Home';
+    showThisElement(SPLASH);
+    if(localStorage.getItem('API-KEY') != null){
+      await populateRooms();
+    }
+  }
+  else if(path == "/profile"){
+    document.title = 'Signup and Update';
+    showThisElement(PROFILE);
+  }
+  else if(path.startsWith("/room/")) {
+    document.title = 'Rooms';
+    showThisElement(ROOM);
+
+    currentRoomID = path.split('/')[2];
+    document.title = 'Room ' + currentRoomID;
+    await populateRooms();
+    document.querySelector('.displayRoomName strong').innerHTML = rooms[currentRoomID]['name'];
+    document.querySelector('#roomNameInput').value = rooms[currentRoomID]['name'];
+    document.querySelector('.roomDetail #roomId').innerHTML = '/rooms/' + currentRoomID;
+  }
+  else if(path == "/login") {
+    document.title = 'Login';
+    showThisElement(LOGIN);
+  } 
+  else {
+    await performRequest(API_ENDPOINTS.ERROR_URL, {}, {}, 'POST');
+  }
+}
+window.addEventListener("DOMContentLoaded", router);
+window.addEventListener("popstate", router);
+
+async function startMessagePolling() {
+  setInterval(async () => {
+    if (currentRoomID === 0) return;
+    
+    getAllMsgsRequest.room_id = currentRoomID;
+    let messages = await performRequest(API_ENDPOINTS.MESSAGES_URL, getAllMsgsRequest, {}, 'GET');
+    let messagesDiv = document.body.querySelector(".messages");
+    
+    while (messagesDiv.firstChild) {
+      messagesDiv.removeChild(messagesDiv.firstChild);
+    }
+  
+    Object.entries(messages).forEach(([key, messageInfo]) => {
+      let messageElement = document.createElement("message");
+      let authorElement = document.createElement("author");
+      authorElement.innerHTML = messageInfo.name;
+      let contentElement = document.createElement("content");
+      contentElement.innerHTML = messageInfo.body;
+      messageElement.appendChild(authorElement);
+      messageElement.appendChild(contentElement);
+      messagesDiv.append(messageElement);
+    });
+  }, 500);
+  return;
+}
+
